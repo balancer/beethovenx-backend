503809ef63a8cc8fe126d172fb494c0357afabc4
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2024-12-06 15:22:14 -0300

    Revert filename case change

--- modules/sor/sorV2/lib/poolsV2/index.ts
+++ modules/sor/sorV2/lib/poolsV2/index.ts
@@ -3,5 +3,5 @@ export * from './fx/fxPool';
 export * from './gyro2/gyro2Pool';
 export * from './gyro3/gyro3Pool';
 export * from './gyroE/gyroEPool';
-export * from './metaStable/metaStablePool';
+export * from './metastable/metastablePool';
 export * from './weighted/weightedPool';
