a689a51d1e71cb88733c5e5c0f7ec6e6ee979a9b
mkflow27 <mkflow27@gmail.com>
2024-12-10 09:55:29 +0100

    style: remove redundancy

--- test/factories/prismaHook.factory.ts
+++ test/factories/prismaHook.factory.ts
@@ -7,23 +7,22 @@ import { HookData } from '../../modules/sources/transformers/hook-transformer';
 class PrismaHookFactory extends Factory<HookData> {}
 
 export const hookFactory = PrismaHookFactory.define(({ params }) => {
-    const hookAddress = params?.address ?? createRandomAddress();
 
     return {
-        address: hookAddress,
-        name: params.name || 'Test Hook',
-        enableHookAdjustedAmounts: params?.enableHookAdjustedAmounts ?? false,
-        shouldCallAfterSwap: params?.shouldCallAfterSwap ?? false,
-        shouldCallBeforeSwap: params?.shouldCallBeforeSwap ?? false,
-        shouldCallAfterInitialize: params?.shouldCallAfterInitialize ?? false,
-        shouldCallBeforeInitialize: params?.shouldCallBeforeInitialize ?? false,
-        shouldCallAfterAddLiquidity: params?.shouldCallAfterAddLiquidity ?? false,
-        shouldCallBeforeAddLiquidity: params?.shouldCallBeforeAddLiquidity ?? false,
-        shouldCallAfterRemoveLiquidity: params?.shouldCallAfterRemoveLiquidity ?? false,
-        shouldCallBeforeRemoveLiquidity: params?.shouldCallBeforeRemoveLiquidity ?? false,
-        shouldCallComputeDynamicSwapFee: params?.shouldCallComputeDynamicSwapFee ?? false,
-        dynamicData: params?.dynamicData ?? {},
-        reviewData: params?.reviewData ?? {
+        address: createRandomAddress(),
+        name: 'Test Hook',
+        enableHookAdjustedAmounts: false,
+        shouldCallAfterSwap: false,
+        shouldCallBeforeSwap: false,
+        shouldCallAfterInitialize: false,
+        shouldCallBeforeInitialize: false,
+        shouldCallAfterAddLiquidity: false,
+        shouldCallBeforeAddLiquidity: false,
+        shouldCallAfterRemoveLiquidity: false,
+        shouldCallBeforeRemoveLiquidity: false,
+        shouldCallComputeDynamicSwapFee: false,
+        dynamicData:  {},
+        reviewData: {
             summary: '',
             reviewFile: '',
             warnings: [],
